84b0eb9f1c73ad06ec02fcee15cb0bfabfa14d35
Oliwier <majoliwier.agh@gmail.com>
2024-05-20 09:00:28 +0200

    prev project base

--- CsharpProject/Controllers/HomeController.cs
+++ CsharpProject/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CsharpProject.Models;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Data.Sqlite;
 
 namespace CsharpProject.Controllers;
 
@@ -8,19 +11,210 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
+     [Route("/")]
     public IActionResult Index()
     {
+        ViewData["Username"] = HttpContext.Session.GetString("Username");
+
         return View();
     }
 
+    [Route("/profile")]
+    public IActionResult Profile()
+    {
+        ViewData["Username"] = HttpContext.Session.GetString("Username");
+        return View();
+    }
+
+    [Route("/privacy")]
     public IActionResult Privacy()
     {
+        ViewData["Username"] = HttpContext.Session.GetString("Username");
         return View();
+    }
+
+    [Route("/logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
+    }
+
+    [Route("/login")]
+    public IActionResult Login()
+    {
+        ViewData["Username"] = HttpContext.Session.GetString("Username");
+        return View();
+    }
+
+    [HttpPost]
+    [Route("/login")]
+    public IActionResult Login(IFormCollection form)
+    {
+        if (form is null)
+            return View();
+
+        using (var connection = new SqliteConnection("Data Source=db.db"))
+        {
+            String username = form["username"].ToString();
+            String password = form["password"].ToString();
+
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM Users WHERE Username = @Username AND Password = @Password;";
+            command.Parameters.AddWithValue("@Username", form["username"].ToString());
+            command.Parameters.AddWithValue("@Password", MD5Hash(form["password"].ToString()));
+
+            var reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                ViewData["Username"] = username;
+                HttpContext.Session.SetString("Username", username);
+                return RedirectToAction("Profile");
+            }
+            else
+            {
+                ViewData["Message"] = "Invalid username or password";
+            }
+
+        }
+        return View();
+    }
+
+    [Route("/register")]
+    public IActionResult Register()
+    {
+        ViewData["Username"] = HttpContext.Session.GetString("Username");
+        return View();
+    }
+
+    [HttpPost]
+    [Route("/register")]
+    public IActionResult Register(IFormCollection form)
+    {
+        if (form is null)
+            return View();
+
+        using (var connection = new SqliteConnection("Data Source=db.db"))
+
+        {
+
+            String username = form["username"].ToString();
+            String password = form["password1"].ToString();
+
+            connection.Open();
+            if (password != form["password2"].ToString())
+            {
+                ViewData["Message"] = "Passwords do not match";
+                return View();
+            }
+
+            var isRegistered = connection.CreateCommand();
+            isRegistered.CommandText = "SELECT COUNT(*) FROM Users WHERE Username = @Username;";
+            isRegistered.Parameters.AddWithValue("@Username", username);
+            var reader = isRegistered.ExecuteReader();
+            if (reader.Read())
+            {
+                if (reader.GetInt32(0) > 0)
+                {
+                    ViewData["Message"] = "Username already exists";
+                    return View();
+                }
+            }
+
+            var command = connection.CreateCommand();
+            command.CommandText = "INSERT INTO Users (Username, Password) VALUES (@Username, @Password);";
+            command.Parameters.AddWithValue("@Username", username);
+            command.Parameters.AddWithValue("@Password", MD5Hash(password));
+
+            command.ExecuteNonQuery();
+
+            ViewData["Message"] = "Registration successful";
+        }
+
+        return View();
+    }
+
+
+
+    private string MD5Hash(string input)
+    {
+        using (var md5 = MD5.Create())
+        {
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    [Route("/data")]
+    public IActionResult Data()
+    {
+        ViewData["Username"] = HttpContext.Session.GetString("Username");
+
+        if (ViewData["Username"] is null)
+        {
+            return RedirectToAction("Login");
+        }
+
+        using (var connection = new SqliteConnection("Data Source=db.db"))
+
+        {
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM Data WHERE UserId = (SELECT Id FROM Users WHERE Username = @Username);";
+            command.Parameters.AddWithValue("@Username", ViewData["Username"].ToString());
+            var reader = command.ExecuteReader();
+            List<String> dataList = new List<String>();
+            while (reader.Read())
+            {
+                dataList.Add(new String(reader.GetString(1)));
+            }
+            ViewData["DataList"] = dataList;
+        }
+        return View();
+    }
+
+    [HttpPost]
+    [Route("/data")]
+    public IActionResult Data(IFormCollection form)
+    {
+        ViewData["Username"] = HttpContext.Session.GetString("Username");
+
+        if (ViewData["Username"] is null)
+        {
+            return RedirectToAction("Login");
+        }
+
+        using (var connection = new SqliteConnection("Data Source=db.db"))
+
+        {
+            String content = form["data"].ToString();
+
+
+            String username = HttpContext.Session.GetString("Username");
+            
+
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = "INSERT INTO Data (Content, UserId) VALUES (@Content, (SELECT Id FROM Users WHERE Username = @Username));";
+            command.Parameters.AddWithValue("@Username", username);
+            command.Parameters.AddWithValue("@Content", content);
+            command.ExecuteNonQuery();
+
+            ViewData["Message"] = "Data added successfully";
+
+        }
+
+        return Data();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -1,15 +1,24 @@
+using Microsoft.Data.Sqlite;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -20,8 +29,48 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseSession();
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=login}/{id?}");
+app.UseStatusCodePagesWithReExecute("/");
+
+app.Use(async (ctx, next) =>
+{
+    await next();
+
+    if ((ctx.Response.StatusCode == 404 || ctx.Response.StatusCode == 400) && !ctx.Response.HasStarted)
+    {
+        string originalPath = ctx.Request.Path.Value ?? "";
+        ctx.Items["originalPath"] = originalPath;
+        ctx.Request.Path = "/login/";
+        ctx.Response.Redirect("/login/");
+        await next();
+    }
+});
 
 app.Run();
+
+
+
+
+var connectionStringBuilder = new SqliteConnectionStringBuilder();
+connectionStringBuilder.DataSource = "db.db";
+
+using var connection = new SqliteConnection(connectionStringBuilder.ConnectionString);
+connection.Open();
+var Command = connection.CreateCommand();
+
+string createTable = "CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY AUTOINCREMENT, Username TEXT NOT NULL, Password TEXT NOT NULL);";
+Command.CommandText = createTable;
+Command.ExecuteNonQuery();
+
+string dataTable = "CREATE TABLE IF NOT EXISTS Data (id INTEGER PRIMARY KEY AUTOINCREMENT, Content TEXT NOT NULL, UserId INTEGER NOT NULL REFERENCES Users(id));";
+
+Command.CommandText = dataTable;
+Command.ExecuteNonQuery();
+
+connection.Close();
+
+
