6b2c36d1fdf94a9d53f897a4e1670c80a728321c
nkwiecienn <natalia.kwiecien.nk@gmail.com>
2024-05-24 15:23:06 +0200

    Added adding songs to playlists from favorites

--- CsharpProject/Controllers/FavoritesController.cs
+++ CsharpProject/Controllers/FavoritesController.cs
@@ -10,5 +10,22 @@ public class FavoritesController : Controller
         var favorites = DataBaseSelect.SelectUsersFavorites(userID);
 
         return View(favorites);
-    } 
+    }
+
+    public IActionResult AddToPlaylist(int id) {
+        ViewData["Username"] = HttpContext.Session.GetString("Username");
+        int userID = (int)HttpContext.Session.GetInt32("UserID");
+        var playlists = DataBaseSelect.SelectUsersPlaylists(userID);
+
+        ViewBag.SongID = id;
+
+        return View(playlists);
+    }  
+
+    [HttpPost]
+    public IActionResult Add (int playlistID, int songID) {
+        ViewData["Username"] = HttpContext.Session.GetString("Username");
+        DataBaseInsertStatements.InsertIntoContent(playlistID, songID);
+        return RedirectToAction("Index");
+    }
 }
